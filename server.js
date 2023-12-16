597205700fad4718f95398c86990200c477884e3
mackwick <mackenziewicker@gmail.com>
2023-12-16 16:35:11 -0500

    added route to drinks JSON file

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
-//********Dependencies
+//********Dependencies */
 const express = require("express")
 const app = express()
+const drinks = require("./models/drinks.js")
+
+
+//********Global variables */
 const port = 3000
 
 
-//********Routes
+//********Routes */
 
-//To root
+//Root route
 app.get("/", (req, res) => {
     res.send("<h1>Welcome to the Gitpub App!</h1>")
 })
 
+//JSON db route
+app.get("/drinks", (req, res) => {
+    res.send(drinks)
+})
 
-//********Server listen
+
+//********Server listen */
 app.listen(port, () => {
     console.log(`Listening on ${port}`)
 })
